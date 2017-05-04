6137d63708dca64f2bae1b7ef09c6c9c51534fbb
wrhong7 <whong@outlook.com>
2017-05-04 16:41:40 -0400

    index page responsive design has been updated + test page registration page front-end is up and ready

--- testBoard.js
+++ testBoard.js
@@ -49,15 +49,130 @@ const deptExperienceList = [
 ]
 
 const partOrFullTimeList = [
-  "Long Term Part Time",
-  "Long Term Full Time",
-  "Short Term Part Time",
-  "Short Term Full Time",
-  "Per Diem",
-  "On Call",
-  "Telehealth",
-  "Traveling Nurse"
+  "Long Term",
+  "Short Term",
+  "5-10 hr per week",
+  "10-20 hr per week",
+  "20-30 hr per week",
+  "30-40 hr per week",
+  "40-50 hr per week",
+  "50+ hr per week",
 ]
+
+const desiredShiftList = [
+  "Day", 
+  "Night",
+  "Weekday",
+  "Weekend"
+]
+
+const driveMissionList = [
+  "Better Hours",
+  "Flexibility",
+  "Less Workload",
+  "Non-profit",
+  "Research",
+  "Academia",
+  "Minority Health",
+  "Underserved Health",
+  "Women's Health",
+  "Veterans",
+  "Children's Health",
+  "School Nurse",
+  "Social Cause",
+  "Municipalities",
+  "Mental Health"
+]
+
+const nursingDegreeList = [
+  "Associates, ASN",
+  "Bachelors, BSN",
+  "Masters, MSN, Clinical Nurse Leader, CNL",
+  "Masters, MSN, School Nursing",
+  "Masters, MSN, Nurse Educator",
+  "Masters, MSN, Certified Nurse Anesthetist, CRNA",
+  "Masters, MSN, Clinical Nurse Specialist, CNS",
+  "Masters, MSN, Certified Nurse Midwife, CNM",
+  "Masters, MSN, Nurse Practitioner, NP",
+  "Doctor of Nursing, ND",
+  "Doctor of Nursing Practice, DNP",
+  "Doctor of Nursing Science, DNSc",
+  "Doctor of Nursing Philosophy, PhD"
+]
+
+const nursingYrExperienceList = [
+  "0-1 Year",
+  "1-2 Years",
+  "2-5 Years",
+  "5-10 Years",
+  "10-15 Years",
+  "15-20 Years",
+  "20+ Years"
+]
+
+const desiredCompensationList = [
+  "$20-25 per hr, $41.6k-52.0k per yr",
+  "$25-30 per hr, $52.0k-62.4k per yr",
+  "$30-35 per hr, $62.4k-72.8k per yr",
+  "$35-40 per hr, $72.8k-83.2k per yr",
+  "$40-45 per hr, $83.2k-93.6k per yr",
+  "$45-50 per hr, $93.6k-104k per yr",
+  "$50-55 per hr, $104k-114k per yr",
+  "$55-60 per hr, $114k-125k per yr",
+  "$60-65 per hr, $125k-135k per yr",
+  "$65-70 per hr, $135k-146k per yr",
+  "$70-75 per hr, $146k-156k per yr",
+  "$75-80 per hr, $156k-166k per yr",
+  "$80+ per hr, 166k+ per yr"
+]
+
+const noList = [
+  "Weekend Shift",
+  "Friday Shift",
+  "Night Shift",
+  "On Call",
+  "Emergency Unit Duty",
+  "Travel",
+  "Unsteady Schedule",
+  "High Patient Load",
+  "Community Clinic"
+]
+
+const languageList = [
+    "Chinese, Mandarin",
+    "Chinese, Cantonese",
+    "Chinese, Fujianese",
+    "Chinese, Shanghainese",
+    "Spanish",
+    "Hindi",
+    "Urdu",
+    "Arabic",
+    "Portugese",
+    "Bengali",
+    "Russian",
+    "Japanese",
+    "Punjabi",
+    "German",
+    "Malay",
+    "Vietnamese",
+    "Korean",
+    "French",
+    "Tamil",
+    "Urdu",
+    "Turkish",
+    "Italian",
+    "Thai",
+    "Gujarati",
+    "Persian",
+    "Polish",
+    "Romanian",
+    "Dutch",
+    "Serbo-Croatian",
+    "Nepali",
+    "Hungarian",
+    "Greek",
+    "Kazkh"
+  ]
 
 $(document).ready(function() {
 
@@ -101,7 +216,83 @@ $(document).ready(function() {
   })
 
   // <!-- Questionnaire 7 Desired shift -->
+  $(".desiredShift").select2({
+    placeholder: "Desired Shift"
+  });
 
+  desiredShiftList.forEach(function(element) {
+    $(".desiredShift").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
 
+  // <!-- Questionnaire 9 Drive/mission -->
+  $(".driveMission").select2({
+    placeholder: "I am passionate about"
+  });
+
+  driveMissionList.forEach(function(element) {
+    $(".driveMission").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
+
+  // <!-- Questionnaire 2 Nursing Degree-->
+  $(".nurseDegree").select2({
+    placeholder: "Degrees"
+  });
+
+  nursingDegreeList.forEach(function(element) {
+    $(".nurseDegree").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
+
+  //      <!-- Questionnaire 4 Nursing Experience -->
+
+  $(".nursingYrExperience").select2({
+    placeholder: "Years of Professional Experience"
+  });
+
+  nursingYrExperienceList.forEach(function(element) {
+    $(".nursingYrExperience").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
+
+  //<!-- Questionnaire 6 Desired compensation -->
+
+  $(".desiredCompensation").select2({
+    placeholder: "Desired Compensation"
+  });
+
+  desiredCompensationList.forEach(function(element) {
+    $(".desiredCompensation").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
+
+  //<!-- Questionnaire 10 No List -->
+  $(".noList").select2({
+    placeholder: "Things I don't like"
+  });
+
+  noList.forEach(function(element) {
+    $(".noList").append(
+      `
+       <option value="${element}">${element}</option>
+      `
+    )
+  })
 
 });
