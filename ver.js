37b48bf1a6e858801acd8e579bdc8894707fba76
wrhong7 <whong@outlook.com>
2017-05-12 18:44:21 -0400

    pushing updated elements for the responsive design

--- ver.js
+++ ver.js
@@ -1,16 +1,21 @@
+//1. Confirmation: if the number entered is a valid cellphone number.
+
 function escapeRegExp(str) {
     return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 }
 
 function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+ 	return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
 function cellValidationSuccess() {
 	$(".cellphoneServNotif").empty();
+	$(".notificationGreen").css("margin-left", "40px;")
 	$(".cellphoneServNotif").append(
 		`<div class="codeSentNotification">
-			Validation code has been sent to ${enteredCellNumber} Please enter the code.
+			<div class="notificationGreen">
+				&#10004; The code has been sent to ${enteredCellNumber}.<br>Please check your phone.
+			</div>
 		</div>
 		`
 	);
@@ -26,37 +31,58 @@ function cellValidationSuccess() {
 		$(".cellDataEntryContainer").empty();
 		$(".cellDataEntryContainer").append(
 			`
-			<input 
-				class="codeDataEntry"
-				placeholder="Enter code"
-			>
-			</input>
-			<div 
-				class="codeDataEntryButton"
-				onclick="sendValidationCode()"
-			>
-				Enter Code
+			<div class="containerUpperDeck">
+				<input 
+					class="codeDataEntry"
+					placeholder="Enter code"
+				>
+				</input>
+				<div 
+					class="codeDataEntryButton"
+					onclick="codeValidation()"
+				>
+					Enter Code
+				</div>
 			</div>
-			<div 
-				class="codeDataEntryButton"
-				onclick="sendValidationCode()"
-			>
-				Resend
+			<div class="containerLowerDeck">
+				<div 
+					class="resendButton"
+					onclick="reSendValidationCode()"
+				>
+					Resend Code
+				</div>
+				<div 
+					class="changePhoneNumberButton"
+					onclick="changePhoneNumber()"
+				>
+					Change Phone Number
+				</div>
 			</div>
 			`
 		)
-	}, 5000)
+	}, 3000)
 }
 
 function cellValidationFailure() {
 	$(".cellDataEntry").empty();
 	$(".cellphoneServNotif").empty();
-	$(".cellphoneServNotif").append(
-		`<div class="codeSentNotification">
-			${enteredCellNumber} is not a valid cellphone number.</br>Please check again and enter the phone number above.
-		</div>
-		`
-	);
+
+	if (enteredCellNumber == "") {
+		$(".cellphoneServNotif").append(
+			`<div class="codeSentNotification">
+				Cellphone field is empty.
+			</div>
+			`
+		)
+	} else {
+		$(".cellphoneServNotif").append(
+			`<div class="codeSentNotification">
+				${enteredCellNumber} is not a valid cellphone number.</br>Please check again and enter the phone number above.
+			</div>
+			`
+		);
+	}
+
 }
 
 var enteredCellNumber;
@@ -84,5 +110,59 @@ function sendValidationCode() {
 	}
 
 }
+
+function reSendValidationCode() {
+	$(".resendButton").empty();
+	$(".resendButton").append(
+		"&#10004; Code Resent"
+	);
+	$(".resendButton").css("background-color", "rgba(60, 242, 60, 0.5)");
+	console.log(enteredCellNumber);
+
+}
+
+function changePhoneNumber() {
+	$(".cellDataEntryContainer").empty();
+	$(".cellDataEntryContainer").append(
+		`
+		<div class="cellDataEntryContainer">
+			<input 
+				class="cellDataEntry"
+				placeholder="Your cellpone number"
+			>
+			</input>
+			<div 
+				class="cellDataEntryButton"
+				onclick="sendValidationCode()"
+			>
+				validate
+			</div>
+		</div>
+		`
+	)
+}
+
+// 2. confirmation if the validation code sent out is matching and accurate.
+
+var userSubmittedCode;
+
+function codeValidation() {
+
+	userSubmittedCode = $(".codeDataEntry").val()
+
+	if (userSubmittedCode == "123EWQ") {
+
+		$(".verificationCover").append(
+			`
+			<div class="confirmVerificationButton">Confirm and<br>Look for Open Positions</div>
+			`
+		)
+
+	}
+
+}
+
+
+
 
 
