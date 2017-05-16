a7c7255e0783b23abacd6e91b3a3be29c9098d04
wrhong7 <whong@outlook.com>
2017-05-16 14:18:31 -0400

    nurse job board has bbeen completed

--- jobBoard.js
+++ jobBoard.js
@@ -1,1 +1,27 @@
-jobBoard.js
+var nurseCustomSearchSetting = true
+
+function nurseCustomSearchSwitch() {
+	if (nurseCustomSearchSetting == true) {
+		nurseCustomSearchSetting = false;
+		$(".jobPostNumber").empty();
+		$(".jobPostNumber").append(
+			`
+				Showing 130 results - your search is <b>not personalized</b> based on your resume <span class="turnOffCustomSearch" onclick="nurseCustomSearchSwitch()">turn on personalization</span> 
+			`
+		);
+	} else {
+		nurseCustomSearchSetting = true;
+		$(".jobPostNumber").empty();
+		$(".jobPostNumber").append(
+			`
+				Showing 29 results - your search is <b>personalized</b> based on your resume <span class="turnOffCustomSearch" onclick="nurseCustomSearchSwitch()">turn off personalization</span>
+			`
+		);	
+	}
+}
+
+$(document).ready(function(){
+    $(".moreSearchCriteriaButton").click(function(){
+        $(".moreSearchCriteriaContainer").toggle(1000);
+    });
+});
