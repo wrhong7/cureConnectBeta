661513a6bd390b45a3096057967ff67566adc114
wrhong7 <whong@outlook.com>
2017-05-24 09:17:33 -0400

    color scheme bug has been fixed

--- howItWorks.js
+++ howItWorks.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var illustrationInProgress = false;
 
 $(document).ready(function() {
@@ -47,45 +46,45 @@ $(document).ready(function() {
 		}
 	})
 })
-=======
-$(document).ready(function() {
+// =======
+// $(document).ready(function() {
 
 
-
-setTimeout(function(){
-	$(".initialStage").hide();
-	$(".firstStage").css("display", "block");
-}, 3000)
-
-setTimeout(function(){
-	$(".firstStage").hide();
-	$(".secondStage").css("display", "block");
-}, 6000)
-
-setTimeout(function(){
-	$(".secondStage").hide();
-	$(".thirdStage").css("display", "block");
-}, 9000)
-
-setTimeout(function(){
-	$(".thirdStage").hide();
-	$(".fourthStage").css("display", "block");
-}, 12000)
 
 // setTimeout(function(){
-// 	$(".fourthStage").hide();
-// 	$(".fifthStage").css("display", "block");
-// }, 10000)
+// 	$(".initialStage").hide();
+// 	$(".firstStage").css("display", "block");
+// }, 3000)
 
 // setTimeout(function(){
-// 	$(".fifthStage").hide();
-// 	$(".sixthStage").css("display", "block");
+// 	$(".firstStage").hide();
+// 	$(".secondStage").css("display", "block");
+// }, 6000)
+
+// setTimeout(function(){
+// 	$(".secondStage").hide();
+// 	$(".thirdStage").css("display", "block");
+// }, 9000)
+
+// setTimeout(function(){
+// 	$(".thirdStage").hide();
+// 	$(".fourthStage").css("display", "block");
 // }, 12000)
 
-// setTimeout(function(){
-// 	$(".sixthStage").hide();
-// 	$(".seventhStage").css("display", "block");
-// }, 14000)
+// // setTimeout(function(){
+// // 	$(".fourthStage").hide();
+// // 	$(".fifthStage").css("display", "block");
+// // }, 10000)
 
-})
->>>>>>> b51d513ea780ae1652f8385110fe4b4501d0ef8f
+// // setTimeout(function(){
+// // 	$(".fifthStage").hide();
+// // 	$(".sixthStage").css("display", "block");
+// // }, 12000)
+
+// // setTimeout(function(){
+// // 	$(".sixthStage").hide();
+// // 	$(".seventhStage").css("display", "block");
+// // }, 14000)
+
+// })
+
